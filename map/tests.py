b51d81a2a08be673ba51c52c1930a81be78b1c56
kelvinadeu <kelvinadeu25@gmail.com>
2019-03-22 14:40:51 +0300

    added test

--- map/tests.py
+++ map/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Comments, Image, Profile
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-# Create your tests here.
+
+class Comment(TestCase):
+
+    def setUp(self):
+
+        self.loise = User.objects.create(username="loise")
+        self.picture = Image.objects.create(image='image1', user=self.loise)
+        self.comment = Review.objects.create(comment = 'so ugly')
+
+        self.test_review = Review.objects.create(user=self.loise, image=self.image1, comment='so ugly')
+        self.test_review.save()
+
+    #Testing instance
+
+    def test_instance(self):
+
+        self.assertTrue(isinstance(self.test_reviews, Review))
